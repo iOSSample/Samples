7d6da85ff50c282a8a546042cbd0b6bd0fa78ce7
duhao1990 <799450576@qq.com>
2015-11-17 14:23:33 +0800

    MyTTTAttributedLabelMyTTTAttributedLabel

---
+++ MyTTTAttributedLabel/MyTTTAttributedLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTTTAttributedLabel
+//
+//  Created by 杜豪 on 15/7/23.
+//  Copyright (c) 2015年 杜豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
